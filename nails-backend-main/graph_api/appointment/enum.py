f3da4312e136954478c6a423379e058b1eed0697
Blustream One <blustreamone@Harshas-MacBook-Pro.local>
2023-12-07 17:45:17 +0530

    new files and folder

--- nails-backend-main/graph_api/appointment/enum.py
+++
@@ -1,6 +0,0 @@
-import graphene
-
-
-class BookingTypeEnum(graphene.Enum):
-    PRODUCT = 'PRODUCT'
-    SERVICE = 'SERVICE'
--- nails-backend-main/graph_api/testimonials/enum.py
+++
@@ -1,8 +0,0 @@
-import graphene
-
-
-class TestimonialTypeEnum(graphene.Enum):
-    CLIENT = 'CLIENT',
-    CELEBRITY = 'CELEBRITY',
-    FRANCHISE = 'FRANCHISE',
-
